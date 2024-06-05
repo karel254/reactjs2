231e47fa823f898f12b2178d5ee4787721ef39e1
karel254 <kajwangbrian420@gmail.com>
2024-06-05 15:25:33 +0300

    Changes to Navbar sizings

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -9,8 +9,8 @@ function Navbar() {
         <Link to={appLinks?.Landing} className="navbar-brand">
           Home
         </Link>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div >
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to={appLinks?.Dashboard}>
                Our Services
