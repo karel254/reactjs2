1864bf1e23e521ab53cb5e929303f806e83de065
karel254 <kajwangbrian420@gmail.com>
2024-06-08 02:10:03 +0300

    navbar fix

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { appLinks } from "../constants/Links";
 import "./Navbar.css";
+import menu from "../assets/Menub.gif";
 function Navbar() {
+  const [showMenu, setShowMenu] = React.useState(false);
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary">
-      <div className="container-fluid bg-primary">
-        <Link to={appLinks?.Landing} className="navbar-brand">
-          Home
-        </Link>
-        <div >
+    <nav className="navbar navbar-expand-lg container bg-light">
+      <div className="desktopMenu">
+  
+       
+        <div className="container-fluid-expand bg-light">
           <ul className="navbar-nav">
+          <li className="nav-item">
+          <Link to={appLinks?.Landing} className="navbar-brand">
+          Home
+          </Link>
+            </li>
+
             <li className="nav-item">
               <Link className="nav-link" to={appLinks?.Dashboard}>
                Our Services
@@ -21,15 +28,25 @@ function Navbar() {
                Meet Our Team
               </Link>
             </li>
+            
             <li className="nav-item">
-             
-              <Link className="nav-link" to={appLinks?.Contact}>
+             <Link className="nav-link" to={appLinks?.Contact}>
                 Contact Us
               </Link>
             </li>
           </ul>
+          </div>
         </div>
-      </div>
+     
+     
+<img src={menu} alt="Menu" className="mobileMenu" onClick={()=>setShowMenu(!showMenu)}/>
+  <div className="navMenu" style={{display: showMenu? 'flex': 'none'}}>
+
+   <Link className="nav-link" to={appLinks?.Landing} onClick={()=>setShowMenu(false)}>Home</Link>
+   <Link className="nav-link" to={appLinks?.Dashboard} onClick={()=>setShowMenu(false)}>Our Services</Link>
+   <Link className="nav-link" to={appLinks?.AboutUs} onClick={()=>setShowMenu(false)}>Meet Our Team</Link>
+   <Link className="nav-link" to={appLinks?.Contact} onClick={()=>setShowMenu(false)}>Contact Us</Link>  
+   </div>
     </nav>
   );
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './Landing.css';
-import bg from  "../assets/Dalle(2).jpg";
 function Landing() {
   return (<section id="landing">
   <div className="landingContent">
       <span className="hello">Hello</span>
       <span className="landingText">We're <span className="landingName">Daltech Software Solutions,</span><br/>Software Engineers✨</span>
-     <div class="bg-primary text-white"><p className="landingPara">Is your organization the next success story? Let Daltech unlock your untapped<br/> potential. The future is coding - don't get left behind!</p>
+     <div class="bg-dark bg-gradient text-white text-wrap"><p className="landingPara">Is your organization the next success story? Let Daltech unlock your untapped<br/> potential. The future is coding - don't get left behind!</p>
       </div>
 
   </div>
-  <img src={bg} alt="Profile" width="1000000" height="100000" className="bg"/>
+  
 
  </section>
   )
